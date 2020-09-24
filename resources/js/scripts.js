67e0bf2aa51030bf2fc15ac08597336d0594af2a
lizstangle <lizstangle@me.com>
2020-09-23 22:31:17 -0700

    deleted duplicate code

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -46,35 +46,35 @@ for (let i=0; i<data.length; ++i) {
     newDiv.appendChild(img)
     console.log(img)
 }
-// the length of our data determines how many times this loop goes around
-for (let i=0; i<data.length; ++i) {
-    // create a new div element and give it a class name
-    let newDiv = document.createElement('div');
-    newDiv.className = 'item'
+// // the length of our data determines how many times this loop goes around
+// for (let i=0; i<data.length; ++i) {
+//     // create a new div element and give it a class name
+//     let newDiv = document.createElement('div');
+//     newDiv.className = 'item'
 
-    // create a paragraph element for a description
-let desc = document.createElement('P')
-// give the paragraph text from the data
-desc.innerText =data[i].desc
-// append the paragraph to the div
-newDiv.appendChild(desc)
-// do the same thing for price
-let price = document.createElement('P')
-price.innerText = data[i].price
-newDiv.appendChild(price)
+//     // create a paragraph element for a description
+// let desc = document.createElement('P')
+// // give the paragraph text from the data
+// desc.innerText =data[i].desc
+// // append the paragraph to the div
+// newDiv.appendChild(desc)
+// // do the same thing for price
+// let price = document.createElement('P')
+// price.innerText = data[i].price
+// newDiv.appendChild(price)
 
-    // create an image element
-    let img = document.createElement('img');
-    // this will change each time we go through the loop. Can you explain why?
-    img.src = data[i].image
-    img.width = 300
-    img.height = 300
+//     // create an image element
+//     let img = document.createElement('img');
+//     // this will change each time we go through the loop. Can you explain why?
+//     img.src = data[i].image
+//     img.width = 300
+//     img.height = 300
 
-    // Add the image to the div
-    newDiv.appendChild(img)
-    // put new div inside items container
-    itemsContainer.appendChild(newDiv)
-}
+//     // Add the image to the div
+//     newDiv.appendChild(img)
+//     // put new div inside items container
+//     itemsContainer.appendChild(newDiv)
+// }
 for (let i=0; i<data.length; ++i) {
     let newDiv = document.createElement('div');
       newDiv.className = 'item'
